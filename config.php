b90ca5bcd0f376a16b417457b2dd4248341974a1
Njoku Okechukwu Valentine <bonsoirval@gmail.com>
2019-10-18 12:51:36 +0100

    gitignore still not working yet

--- config.php
+++ config.php
@@ -23,8 +23,8 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'njoku');
-define('DB_PASSWORD', 'nuhTMX6z');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
 define('DB_DATABASE', 'opencartdev');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
